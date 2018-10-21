e87c09dce3a4f4efa01ad7d4d7b077ab478e0fa8
acschristoph <g4sacs@gmail.com>
2018-10-21 10:53:22 +0200

    Add dynamic counters

--- js/main.js
+++ js/main.js
@@ -13,3 +13,41 @@ var pvue = new Vue({
         produkte: produkte
     }
 })
+
+var cvue = new Vue({
+    el: '#counter_vue',
+    data: {
+        frisuren: 2234,
+        customers: 330,
+        clients: 401,
+        start_year: 2012,
+        days_since_start: days_passed(new Date(2012, 1, 1))
+    },
+    computed: {
+        // a computed getter
+        calculate_years: function () {
+          // `this` points to the vm instance
+          const current = new Date().getFullYear()
+          return current - this.start_year
+        },
+        calculate_clients: function() {
+          console.log(this.days_since_start)
+          return this.days_since_start * 2
+        },
+        calculate_customers: function() {
+            console.log(this.days_since_start)
+            return Math.round( this.days_since_start * 1.05 );
+        },
+        calculate_fix_clients: function() {
+            console.log(this.days_since_start)
+            return Math.round( this.days_since_start * 0.2 );
+        }
+    }
+})
+
+function days_passed(dt) {
+    var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+    var today = new Date();
+    var secondDate = new Date(dt.getTime());
+    return Math.round(Math.abs((today.getTime() - secondDate.getTime())/(oneDay)));  
+  }
